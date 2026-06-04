cd2efa47b320c6542024bb401357784a317c9ec7
HieuLV03 <levanhieu14102k3@gmail.com>
2026-06-04 13:19:13 +0700

    o

--- src/components/Layout/ClientLayout.jsx
+++ src/components/Layout/ClientLayout.jsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 import Header from "@/components/Layout/Header/Header";
-import Footer from "@/components//LayoutFooter/Footer";
+import Footer from "@/components//Layout/Footer/Footer";
 import ZaloButton from "@/components/ZaloButton/ZaloButton";
 import BookingButton from "@/components/BookingButton/BookingButton";
 import BookingPopup from "@/components/BookingPopup/BookingPopup";
