6a727bb37d5f1a47ba8ca5dcc8da3b67440d0691
HieuLV03 <levanhieu14102k3@gmail.com>
2026-06-29 16:01:02 +0700

    u

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -5,82 +5,60 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="footerContainer">
+<footer className="footer">
+  <div className="footerContainer">
 
-        {/* BRAND */}
-        <div className="footerBrand">
-          <h2>THẨM MỸ VIỆN HISU</h2>
-          <p>
-            Nâng tầm nhan sắc với công nghệ thẩm mỹ hiện đại, an toàn và tự nhiên.
-          </p>
-        </div>
+    {/* BRAND */}
+    <div className="footerBrand">
+      <h2>MIREC</h2>
+      <p>
+        Trung tâm kiểm định bản đồ & tư vấn tài nguyên - môi trường.
+      </p>
+    </div>
 
-        {/* COMPANY INFO */}
-        <div className="footerCompany">
-          <h3>CÔNG TY TNHH HISU BEAUTY</h3>
+    {/* INFO */}
+    <div className="footerBox">
+      <h3>Thông tin công ty</h3>
 
-          <p>
-            <strong>🏢 Trụ sở chính:</strong><br />
-            354/47 Quốc lộ 1, P. Bình Tân, TP.HCM
-          </p>
+      <p><strong>🏢 Trụ sở:</strong><br />
+        Số 18 Alexander De Rhodes, TP.HCM
+      </p>
 
-          <p>
-            <strong>🏥 Cơ sở 2:</strong><br />
-            15A Sông Đà, P. Tân Sơn Hòa, TP.HCM
-          </p>
+      <p><strong>🏥 Cơ sở:</strong><br />
+        286 Quách Điêu, TP.HCM
+      </p>
 
-          <p>
-            <strong>📞 Điện thoại:</strong> 0372 089 821
-          </p>
+      <p><strong>📞 Hotline:</strong> (028) 73 034 268</p>
+      <p><strong>📧 Email:</strong> info@mirec.vn</p>
+    </div>
 
-          <p>
-            <strong>📧 Email:</strong> thammyvienhisu@gmail.com
-          </p>
+    {/* LINKS */}
+    <div className="footerBox">
+      <h3>Liên kết</h3>
 
-          <p>
-            <strong>🌐 Website:</strong> www.hisu.com
-          </p>
-        </div>
+      <Link href="/">Trang chủ</Link>
+      <Link href="/services">Dịch vụ</Link>
+      <Link href="/posts">Bài viết</Link>
+      <Link href="/booking">Đặt lịch</Link>
+    </div>
 
-        {/* LINKS */}
-        <div className="footerLinks">
-          <h3>Liên kết</h3>
+    {/* SOCIAL */}
+    <div className="footerBox">
+      <h3>Kết nối</h3>
 
-          <Link href="/">Trang chủ</Link>
-          <Link href="/services">Dịch vụ</Link>
-          <Link href="/posts">Bài viết</Link>
-          <Link href="/booking">Đặt lịch</Link>
-        </div>
+      <p>💬 Hỗ trợ 24/7</p>
 
-        {/* SOCIAL / HOTLINE */}
-        <div className="footerContact">
-          <h3>Hotline hỗ trợ</h3>
-
-          <p>📞 0372 089 821</p>
-
-          <p>💬 Tư vấn 24/7</p>
-
-          <h3>Mạng xã hội</h3>
-
-          <div className="social">
-            <a href="https://www.facebook.com/profile.php?id=61564510092133">
-              Facebook HISU Beauty
-            </a>
-          </div>
-
-          <div className="social">
-            <a href="https://www.facebook.com/profile.php?id=61588684948414">
-              Fanpage dịch vụ
-            </a>
-          </div>
-        </div>
-
+      <div className="social">
+        <a href="#">Facebook</a>
+        <a href="#">Zalo</a>
       </div>
+    </div>
 
-      <div className="footerBottom">
-        © {new Date().getFullYear()} HISU Beauty. All rights reserved.
-      </div>
-    </footer>
+  </div>
+
+  <div className="footerBottom">
+    © {new Date().getFullYear()} MIREC. All rights reserved.
+  </div>
+</footer>
   );
 }
