270934680cd980d2e8332deb883afeb9a82d22a1
HieuLV03 <levanhieu14102k3@gmail.com>
2026-07-05 15:26:48 +0700

    y

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -1,6 +1,8 @@
 export default function Head() {
   return (
     <>
+      <title>Nhà Ngọc</title>
+
       <meta
         name="google-site-verification"
         content="iMhkqfnYHYPZ2e7ZhvNa8URs0nteVRjtS03F9CKa0sU"
