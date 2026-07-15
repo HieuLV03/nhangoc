0d8607301b73e9dfd5dc13dfa0b8c70dc11e7e18
HieuLV03 <levanhieu14102k3@gmail.com>
2026-07-15 21:06:38 +0700

    n

--- src/app/api/ai/product/route.js
+++ src/app/api/ai/product/route.js
@@ -1,54 +1,8 @@
-import { GoogleGenAI } from "@google/genai";
 import { NextResponse } from "next/server";
 
-const ai = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY,
-});
-
-export async function POST(req) {
-  try {
-    const { name } = await req.json();
-
-    const prompt = `
-Viết JSON:
-
-{
-"description":"",
-"content":""
-}
-
-Tên sản phẩm:
-${name}
-`;
-
-    const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: prompt,
-    });
-
-    console.log("===== RESPONSE =====");
-    console.log(response);
-
-    console.log("===== TEXT =====");
-    console.log(response.text);
-
-    return NextResponse.json({
-      success: true,
-      data: response,
-      text: response.text,
-    });
-  } catch (err) {
-    console.error("===== ERROR =====");
-    console.error(err);
-
-    return NextResponse.json(
-      {
-        error: err.message,
-        stack: String(err),
-      },
-      {
-        status: 500,
-      }
-    );
-  }
+export async function POST() {
+  return NextResponse.json({
+    ok: true,
+    message: "Route mới hoạt động",
+  });
 }
