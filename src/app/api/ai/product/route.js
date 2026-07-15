e18d75b33cc4976db3f351bca3bee74a0a53426d
HieuLV03 <levanhieu14102k3@gmail.com>
2026-07-15 21:25:37 +0700

    y

--- src/app/api/ai/product/route.js
+++ src/app/api/ai/product/route.js
@@ -1,12 +1,25 @@
 import { GoogleGenAI } from "@google/genai";
 import { NextResponse } from "next/server";
-
+const { name, type } = await req.json();
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
 
 export async function POST(req) {
   try {
+    const prompt = `
+Viết JSON.
+
+{
+"description":"..."
+}
+
+Tên sản phẩm:
+${name}
+
+Chỉ trả về JSON.
+`;
+
     const { name } = await req.json();
 
     const response = await ai.models.generateContent({
